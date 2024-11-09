486411e2280cc3733bda11ec2ad78a1854af7372
jp-grilo <joaoplopescg@gmail.com>
2024-11-09 18:41:54 -0300

    testando rotas no front

---
+++ src/components/PostList.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const PostList = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5023/api/posts')
+      .then(response => {
+        setPosts(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the posts!', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PostList;
